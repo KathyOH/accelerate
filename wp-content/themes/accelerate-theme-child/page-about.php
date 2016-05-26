0b0403a743219618f2ab6263744f155c6086e53b
Kathy Hoile <kohoile@outlook.com>
2016-05-26 15:48:04 -0400

    updated about and css AND add 404 page

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -1,109 +1,94 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying the About page
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
+ 
  * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
  */
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
+    <?php 
+        $taxi_image = get_field("taxi_image");
+        $taxi_text = get_field("taxi_text"); 
+        $taxi_size = "full"; $services_intro_title = get_field("services_intro_title");
+        $services_intro_text = get_field("services_intro_text");
+        $service_1_title = get_field("service_1_title");
+        $service_1_text = get_field("service_1_text");
+        $service_1_image = get_field("service_1_image");   
+        $service_2_title = get_field("service_2_title");
+        $service_2_text = get_field("service_2_text");
+        $service_2_image = get_field("service_2_image");   
+        $service_3_title = get_field("service_3_title");
+        $service_3_text = get_field("service_3_text");
+        $service_3_image = get_field("service_3_image");   
+        $service_4_title = get_field("service_4_title");
+        $service_4_text = get_field("service_4_text");
+        $service_4_image = get_field("service_4_image");   
+        $contact_button_title = get_field("contact_button_title");
+        $size = "small"; ?> 
             
-                <?php 
-                $services_intro_title = get_field("services_intro_title");
-                $services_intro_text = get_field("services_intro_text");
-                $service_1_title = get_field("service_1_title");
-                $service_1_text = get_field("service_1_text");
-                $service_1_image = get_field("service_1_image");   
-                $service_2_title = get_field("service_2_title");
-                $service_2_text = get_field("service_2_text");
-                $service_2_image = get_field("service_2_image");   
-                $service_3_title = get_field("service_3_title");
-                $service_3_text = get_field("service_3_text");
-                $service_3_image = get_field("service_3_image");   
-                $service_4_title = get_field("service_4_title");
-                $service_4_text = get_field("service_4_text");
-                $service_4_image = get_field("service_4_image");   
-                $contact_button_title = get_field("contact_button_title");
-                $contact_button_text = get_field("contact_button_text");
-                $size = "small"; 
-                $taxi_image = get_field("taxi_image");
-                $taxi_text = get_field("taxi_text"); ?> 
-            
+        <div class="site-content">
             <section class="hero-about">
-                <?php while ( have_posts() ) : the_post(); ?>
-                <div class="hero-text">
-                    <h3><?php the_content(); ?></h3>
+                <div class="hero-text">    
+                    <h3><?php echo $taxi_text; ?></h3>
                 </div>
-                <?php endwhile; //end of the loop. ?>
+            </section>
+                
+            <section class="about-intro">
+                <h6><?php echo $services_intro_title; ?></h6>
+                    <?php echo $services_intro_text; ?>
+            </section>
+                
+            <section class = "about-section">
+                <div class="service-image">
+                    <?php echo wp_get_attachment_image($service_1_image, $size); ?>
+                </div>
+                <div class = "service-description">
+                    <h2><?php echo $service_1_title; ?></h2>
+                        <p><?php echo $service_1_text; ?></p>
+                </div>
+            </section>
+                    
+            <section class = "about-section">
+                <div class = "service-image">
+                    <?php echo wp_get_attachment_image($service_2_image, $size); ?>
+                </div>
+                <div class = "service-description">
+                    <h2><?php echo $service_2_title; ?></h2>
+                        <p> <?php echo $service_2_text; ?></p>
+                </div>
+            </section>
+                
+            <section class = "about-section">
+                <div class = "service-image">
+                    <?php echo wp_get_attachment_image($service_3_image, $size); ?>
+                </div>
+                <div class = "service-description">
+                    <h2><?php echo $service_3_title; ?></h2>
+                        <p> <?php echo $service_3_text; ?></p>
+                </div>
+            </section>
+                
+            <section class = "about-section">
+                <div class = "service-image">
+                    <?php echo wp_get_attachment_image($service_4_image, $size); ?>
+                </div>
+                <div class = "service-description">
+                    <h2><?php echo $service_4_title; ?></h2>
+                        <p><?php echo $service_4_text; ?></p>
+                </div>
+            </section>
+                
+            <section class = "about-section">
+                <div class = "contact-description">
+                    <h2><strong><?php echo $contact_button_title; ?></strong></h2>
+                        <a class="button" href="<?php echo home_url(); ?>/contact">Contact us</a>
+                </div>
             </section>
             
-            <div class="site-content">
-                <?php echo wp_get_attachment_image($taxi_image); ?>
-                <?php echo $taxi_text; ?>
-                
-                <section class="about-intro">
-                <h5><?php echo $services_intro_title; ?></h5>
-                <?php echo $services_intro_text; ?>
-                </section>
-                
-                <section class = "about-section">
-                    <div class="service-image align-left">
-                        <?php echo wp_get_attachment_image($service_1_image, $size); ?>
-                    </div>
-                    <div class = "service-description">
-                        <h2><?php echo $service_1_title; ?></h2>
-                        <p><?php echo $service_1_text; ?></p>
-                    </div>
-                </section>
-                    
-                <section class = "about-section">
-                    <div class = "service-image align-right">
-                        <?php echo wp_get_attachment_image($service_2_image, $size); ?>
-                    </div>
-                    <div class = "service-description">
-                        <h2><?php echo $service_2_title; ?></h2>
-                        <p> <?php echo $service_2_text; ?></p>
-                    </div>
-                </section>
-                
-                <section class = "about-section">
-                    <div class = "service-image align-left">
-                        <?php echo wp_get_attachment_image($service_3_image, $size); ?>
-                    </div>
-                    <div class = "service-description">
-                        <h2><?php echo $service_3_title; ?></h2>
-                        <p> <?php echo $service_3_text; ?></p>
-                    </div>
-                </section>
-                
-                <section class = "about-section">
-                    <div class = "service-image align-right">
-                        <?php echo wp_get_attachment_image($service_4_image, $size); ?>
-                    </div>
-                    <div class = "service-description">
-                        <h2><?php echo $service_4_title; ?></h2>
-                        <p><?php echo $service_4_text; ?></p>
-                    </div>
-                </section>
-                
-                <section class = "about-section">
-                    <div class = "contact-description">
-                        <h2><strong><?php echo $contact_button_title; ?></strong></h2>
-                        <p> <?php echo $contact_button_text; ?></p>
-                    </div>
-                </section>
-            
-            </div><!-- #site-content --> 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+        </div><!-- #site-content -->
 
 <?php get_footer(); ?>
