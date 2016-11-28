7c3ce79df2293cad7f2445f2ddc143ae1d5b2720
Kathy Hoile <kohoile@outlook.com>
2016-11-28 12:29:19 -0500

    updated archive-case_studies page

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -13,41 +13,31 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				$size = "full";
-				$services = get_field('services');
-				$client = get_field('client');
-				$link = get_field('site_link');
+<div id="primary" class="site-content">
+	<div id="content" role="main">
+	<?php while ( have_posts() ) : the_post(); 
+			$image_1 = get_field("image_1");
+			$size = "full";
+			$services = get_field('services'); ?>
 			
 		<article class="case-study">
 			<aside class="case-study-sidebar">
-				<h2><?php the_title(); ?></h2>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				<h6><span style="color: #808080;"><?php echo $services; ?></span></h6>
-				<h5>Client: <?php echo $client; ?></h5>
-				<?php the_content(); ?>
-				<p><strong><a href="<?php echo $link; ?>">Visit Project</a></strong></p>
-			</aside>
-			
 				<?php the_excerpt(); ?>
-			<?php endwhile; // end of the loop. ?>
-			
+				<p><strong><a href="<?php the_permalink(); ?>">View Project</a></strong></p>
+			</aside>
+			<div class="case-study-images">
+				<a href="<?php the_permalink(); ?>">
+            		<?php if($image_1) { 
+                		echo wp_get_attachment_image( $image_1, $size );  
+					} ?>
+				</a> 
+        	</div>                
 		</article>
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
+	<?php endwhile; // end of the loop. ?>
+	
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
-
-
-
-	
-		
-
-			<div class="case-study-images">
-				<?php if ($image_1) { ?>
-				<?php echo wp_get_attachment_image( $image_1, $size ); ?>
-			
-			</div>
