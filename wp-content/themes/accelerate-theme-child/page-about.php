c736568bf4f1417883c6622907551c312b0eb297
Kathy Hoile <kohoile@outlook.com>
2016-12-12 10:12:44 -0500

    working on postitioning About page

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -37,44 +37,56 @@ get_header(); ?>
 	?>
 
 
-			<section class="hero-about">
-				<div class="hero-text">
-					<h3 class="hero-text"><span class="main-color">Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</h3>
-					
-				</div>
-			</section>
+	<section class="hero-about">
+		<div class="hero-text">
+			<h3 class="hero-text"><span class="main-color">Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</h3>
+		</div>
+	</section>
 			
-
-
-
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			
-
-			 <section class="about-intro">
-                <h6><?php echo $services_intro_title; ?></h6>
-                    <?php echo $services_intro_text; ?>
-            </section>
+		<section class="about-intro">
+			<h6><?php echo $services_intro_title; ?></h6>
+                <?php echo $services_intro_text; ?>
+        </section>
 			
 			
+	<section class="about-section">
+		<div class="service-image">
 			<?php echo wp_get_attachment_image($service_1_image, $size); ?>
+		</div>
+		<div class="service-description">
 			<h2><?php echo $service_1_title; ?></h2>
 			<p><?php echo $service_1_description; ?></p>
-			
+		</div>
+		<div class="service-image">
 			<?php echo wp_get_attachment_image($service_2_image, $size); ?>
+		</div>
+		<div class="service-description">
 			<h2><?php echo $service_2_title; ?></h2>
 			<p> <?php echo $service_2_description; ?></p>
-			
+		</div>
+		<div class="service-image">
 			<?php echo wp_get_attachment_image($service_3_image, $size); ?>
+		</div>
+		<div class="service-description">
 			<h2><?php echo $service_3_title; ?></h2>
 			<p> <?php echo $service_3_description; ?></p>
-			
+		</div>
+		<div class="service-image">
 			<?php echo wp_get_attachment_image($service_4_image, $size); ?>
+		</div>
+		<div class="service-description">
 			<h2><?php echo $service_4_title; ?></h2>
 			<p> <?php echo $service_4_description; ?></p>
-			
+		</div>
+	
+		<div class="contact-description">
 			<h2><?php echo $about_contact_title; ?></h2>
 			<?php echo $contact_button_text; ?>
+		</div>
+	</section>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
